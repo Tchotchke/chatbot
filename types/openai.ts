db855b04796fc2af1d53b29e27d640af552be73d
Matthew Teschke <mteschke@gmail.com>
2024-05-13 13:54:15 -0400

    Update openai.ts to gpt-4o

--- types/openai.ts
+++ types/openai.ts
@@ -9,11 +9,11 @@ export interface OpenAIModel {
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_4 = 'gpt-4-turbo',
+  GPT_4 = 'gpt-4o',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_4;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
